ed0db81feb04e19fa1656cc0b785040c5611349f
Andre LaFleur <andre.lafleur@utah.edu>
2019-08-28 23:16:36 -0600

    Setup full build. It all works!

---
+++ README.md
@@ -0,0 +1,13 @@
+# SpigotNotepad
+
+TODO: explain what this is
+
+## TODO/Issues
+
+- [ ] `/note` craps out because it's invalid
+- [ ] `/note read <NAME>` to read the full text of a note
+- [ ] A message for creating a note informing that it's valid
+- [ ] Show messages to player about note command in italics to help player know it's from the plugin
+- [ ] Only show the first maybe 10 characters of a note, along with its name with the `/list` command
+- [ ] A proper intellisense for note command stuffs
+    - [ ] `/note TAB_PRESS` should show add, list, delete
--- src/main/java/usa/alafleur/spigot_notepad/Main.java
+++ src/main/java/usa/alafleur/spigot_notepad/Main.java
@@ -2,6 +2,7 @@ package usa.alafleur.spigot_notepad;
 
 import io.objectbox.BoxStore;
 import org.bukkit.plugin.java.JavaPlugin;
+import usa.alafleur.spigot_notepad.command.NoteCommand;
 import usa.alafleur.spigot_notepad.model.MyObjectBox;
 
 public class Main extends JavaPlugin {
@@ -13,11 +14,12 @@ public class Main extends JavaPlugin {
         super.onEnable();
 
         store = MyObjectBox.builder().name("spigot-notepad-db").build();
+        this.getCommand("note").setExecutor(new NoteCommand(store));
     }
 
     @Override
     public void onDisable() {
-        super.onEnable();
+        super.onDisable();
         store.close();
     }
 }
