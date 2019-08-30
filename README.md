0d3b37feb0414e804c5a74b213d980ba33231d5f
Andre LaFleur <andre.lafleur@utah.edu>
2019-08-30 10:13:19 -0600

    Add another set of aliases

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ TODO: explain what this is
 - [x] `/note read <NAME>` to read the full text of a note
 - [x] A message for creating a note informing that it's valid
 - [ ] Show messages to player about note command in italics to help player know it's from the plugin
-- [ ] Only show the first maybe 10 characters of a note
+- [x] Only show the first maybe 10 characters of a note
 - [ ] A proper intellisense for note command stuffs
-- [ ] Single letter aliases for CRUD operations (a, c, s, l, d)
-    - [ ] `/note TAB_PRESS` should show add, show, list, delete
+- [x] Single letter aliases for CRUD operations (a, c, s, l, d)
--- src/main/java/usa/alafleur/spigot_notepad/command/CommandRequestType.java
+++ src/main/java/usa/alafleur/spigot_notepad/command/CommandRequestType.java
@@ -6,10 +6,10 @@ import java.util.List;
 public enum CommandRequestType {
     ADD_REQUEST, LIST_REQUEST, DELETE_REQUEST, SHOW_REQUEST, INVALID_REQUEST;
 
-    public static final List<String> ADD_ALIASES = Arrays.asList("add", "create", "new");
-    public static final List<String> LIST_ALIASES = Arrays.asList("list");
-    public static final List<String> DELETE_ALIASES = Arrays.asList("delete", "remove");
-    public static final List<String> SHOW_ALIASES = Arrays.asList("show");
+    public static final List<String> ADD_ALIASES = Arrays.asList("add", "create", "new", "a");
+    public static final List<String> LIST_ALIASES = Arrays.asList("list", "l");
+    public static final List<String> DELETE_ALIASES = Arrays.asList("delete", "remove", "d");
+    public static final List<String> SHOW_ALIASES = Arrays.asList("show", "read", "r", "s");
 
     public static CommandRequestType extractValue(String alias) {
         if (ADD_ALIASES.contains(alias.toLowerCase())) {
